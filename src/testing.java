c849a50eeec253b472e70e1ca3c7d212c7c1c04f
siddhant tiwari <153662147+Siddhtiwa@users.noreply.github.com>
2024-03-03 17:39:45 +0530

    static data member

---
+++ src/testing.java
@@ -0,0 +1,20 @@
+public class testing {
+
+    public static void change(Student s) {
+        s.name = "rohan";
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("ranjeet",45,89);
+        System.out.println(s1.numberofstudent);
+
+
+
+
+        Student s2 = new Student("sumit",40,50);
+        System.out.println(s2.numberofstudent);
+
+        Student s3 = new Student("mohan",30,80);
+        System.out.println(s3.numberofstudent);
+    }
+}
